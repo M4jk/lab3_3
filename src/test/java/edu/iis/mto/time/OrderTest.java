74afd0b56df181c9a7161e08a12858c79eef9e79
Kaczmarczyk.Michal <209344@edu.p.lodz.pl>
2019-04-23 17:01:16 +0200

    added testOrderConfirmedInValidPeriodHours test

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,6 +1,9 @@
 package edu.iis.mto.time;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
 
 public class OrderTest {
 
@@ -9,6 +12,15 @@ public class OrderTest {
         Order order = new Order(new FakeClock());
         order.submit();
         order.confirm();
+    }
+
+    @Test
+    public void testOrderConfirmedInValidPeriodHours() {
+        Order order = new Order(new RealClock());
+        order.submit();
+        order.confirm();
+
+        Assert.assertThat(order.getOrderState(), is(Order.State.SUBMITTED));
     }
 
 }
